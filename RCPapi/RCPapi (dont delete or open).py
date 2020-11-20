9637e48a5ee23ce005cd1f93e6b1dd0961b9c853
Kai-Builder <gkai@gmail.com>
2020-11-19 19:52:36 -0500

    added new classes for api reference

--- RCPapi/RCPapi (dont delete or open).py
+++ RCPapi/RCPapi (dont delete or open).py
@@ -21,3 +21,24 @@ class Prompt(object): # This is where all prompt Functionality is stored.
     def WaitFor(secs, after):
         time.sleep(secs)
         after
+
+
+class API_LOG():
+    def CreateOnAction(action, vals):
+        print('Waiting For' + action)
+        time.sleep(3)
+        action
+        time.sleep(1)
+        vals
+    def log(log, logChildren):
+        a = open(log, "w")
+        a.write(logChildren)
+        a.close()
+
+
+class APIReference():
+    class Sheet(object):
+        s_contents = 'The RCPAPI is a lightweight language built from thr ground up, for speed, time, and emotion. Where does the emotion come in?, I Dont even know'
+        s_helpcommand = 'RC_Help'
+
+
